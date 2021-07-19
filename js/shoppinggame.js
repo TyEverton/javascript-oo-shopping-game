5b89db501c89a4b56f7e2d66c7d6a9a0592361c0
Ty Everton <everton_915@yahoo.com>
2021-07-19 17:00:16 -0600

    tryign to fix the class product

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -19,12 +19,12 @@ let player = {
     }
 }
 // Define the Product class - write the Constructor function for Product class here
-function Product(id, name, price, expiryDate) {
-
-        this.id = id,
-        this.name = name,
-        this.price = price,
-        this.expiryDate = expiryDate
+class Product { 
+    constructor();
+        id;
+        name;
+        price;
+        expiryDate
     }
 
 // Complete the dateDiff function
